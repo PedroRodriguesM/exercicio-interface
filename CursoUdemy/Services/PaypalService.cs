1267f6c079c9eb9fc70b198a3b24612bb2aae9d4
Pedro Rodrigues <pedrorodriguesmoraesdasilva@outlook.com>
2021-10-27 18:57:32 -0300

    Add project files.

---
+++ CursoUdemy/Services/PaypalService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoUdemy.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        private const double _monthlySimpleInterest = 0.01;
+        private const double _feePercentage = 0.02;
+
+        public double PaymentFee(double amount)
+        {
+            return amount * _feePercentage;
+        }
+
+        public double Interest(double amount, int months)
+        {
+            return amount * _monthlySimpleInterest * months;
+        }
+    }
+}
